eab682f6102c7749eb2a0b783b1bff2d767ca668
Alain TRAN <alain.tran@opsone.net>
2024-01-08 10:15:13 +0100

    add admin CRUD resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AdminModule],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
